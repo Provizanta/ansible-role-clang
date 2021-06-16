51b4fc5151f8ddbbc1625257aa80e51ffc2d98cd
nre <nre@ableton.com>
2021-06-16 12:14:09 +0200

    Add some sanity tests for Molecule to run

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_clang_versions(host):
+    for clang_version in ['10']:
+        clang_cc = host.file(f'/usr/bin/clang-{clang_version}')
+        clang_cpp = host.file(f'/usr/bin/clang++-{clang_version}')
+        clang_lld = host.file(f'/usr/bin/lld-{clang_version}')
+
+        assert clang_cc.is_file
+        assert clang_cpp.is_file
+        assert clang_lld.is_file
